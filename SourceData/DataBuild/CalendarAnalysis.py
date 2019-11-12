a7554d145085b189f4d4d4d58e94466e9a5b0e41
alex <alexciccolella125>
2019-11-11 20:50:52 -0500

    Started cleaning up data to create listing dataframe. attempting top plot the data with mathplotlib

---
+++ SourceData/DataBuild/CalendarAnalysis.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+Calendar = pd.read_csv("../bostonAirbnb/calendar.csv")
+# print(Calendar.listing_id.unique())
+
+#I am looking for the average number of days a listing is available for
+
+#number of items in index = 1308890
+#print(len(Calendar.index))
+
+
+# CalObj = Calendar.apply(lambda x: True if x['listing_id'] != 365 else False, axis=1)
+# #count number of true in series
+# numOfTrue = len(CalObj[CalObj == True].index)
+#
+# print(numOfTrue)
+#
+# CalOBJ = Calendar.groupby('listing_id').count()
+# Calendar_not_365 = CalOBJ[CalOBJ.date > 365]
+# print(Calendar_not_365)
+
+print(Calendar[Calendar.listing_id.unique < 365])
+
+#
+# Calendar_not_365 = Calendar[Calendar.listing_id == 12898806]
+# print(Calendar_not_365)
+
+
+"""      state review_scores_rating review_scores_accuracy
+1490     FL                   95                      9
+1879     FL                   90                      9
+1999     FL                   95                     10
+2451     FL                   96                     10
+4840     FL                  100                     10
+5973     FL                  NaN                    NaN
+6314     FL                  100                     10
+6329     FL                  100                     10
+6472     FL                  100                     10
+6473     FL                  100                     10
+6519     FL                   95                      9
+6794     FL                   73                      9
+8255     FL                  NaN                    NaN
+9075     FL                   60                     10
+10104    FL                  NaN                    NaN"""
--- SourceData/DataBuild/comparingHostAndGender.py
+++ SourceData/DataBuild/comparingHostAndGender.py
@@ -11,8 +11,46 @@ with open("../NameGenderProb/name_gender.csv", newline='') as csvfile:
 nameGender = pd.read_csv("../NameGenderProb/name_gender.csv")
 Listings = pd.read_csv("../bostonAirbnb/listings.csv")
 
-print(Listings[~Listings.host_name.isin(nameGender.name)].host_name)
+#print(Listings[~Listings.host_name.isin(nameGender.name)].host_name)
 #returns a list of names not in the table (3583 thousand)
 #print(Listings[Listings.host_name.isin(nameGender.name)].host_name)
 
-#print(sum(1 for row in Listings['host_id'].unique()))
+print(sum(1 for row in Listings['host_id'].unique()))
+
+
+"""types of analysis {
+    linear regression
+    logistic regression
+    k-nearest neighbors
+    neural networks
+    classification and regression trees
+    cluster analysis
+}"""
+
+
+
+
+
+
+
+
+
+
+"""
+
+Message for Gaby***
+Hello mayor Hunschofsky,
+
+It was a pleasure speaking with you at the Isreal Dinner at FAU. 
+Your work with the students of parkland is inspiring. 
+Given my past experience with gun violence as a child, 
+I have always been motivated to take action and help others who have become victims as well.
+My ultimate goal is to become a LMHC specialised in PTSD and trauma.
+
+Following up with out conversation, you had mentioned that you would be able to help me get in contact with trauma 
+specialists in Broward county. I really hope to use my skills as a psychology major to make a difference.
+
+Thank you again for all your time and help you give to our community,
+Gabriella Pettit.
+
+"""
